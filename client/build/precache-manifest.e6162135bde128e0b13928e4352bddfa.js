0a2bbcaf4a155b4b632c936f5c8bb59c3bf34340
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-06-30 11:26:44 -0700

    Pages and components folders established

---
+++ client/build/precache-manifest.e6162135bde128e0b13928e4352bddfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0e9adfbbcb4709a6325",
+    "url": "/new-mern-practice/static/css/main.9a555ecc.chunk.css"
+  },
+  {
+    "revision": "d0e9adfbbcb4709a6325",
+    "url": "/new-mern-practice/static/js/main.e0e42d4b.chunk.js"
+  },
+  {
+    "revision": "c5893241a6eb6858c7e0",
+    "url": "/new-mern-practice/static/js/runtime~main.32ec5198.js"
+  },
+  {
+    "revision": "b833d4679e575f81df15",
+    "url": "/new-mern-practice/static/js/2.c1899c9d.chunk.js"
+  },
+  {
+    "revision": "ab2c3997a259adc09a47f03f7f920b16",
+    "url": "/new-mern-practice/static/media/profilePicture.ab2c3997.jpg"
+  },
+  {
+    "revision": "a1870a96174e74719275c4a75bd1d014",
+    "url": "/new-mern-practice/index.html"
+  }
+];
