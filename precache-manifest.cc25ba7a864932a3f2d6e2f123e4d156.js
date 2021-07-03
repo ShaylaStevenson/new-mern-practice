90b21402c2b5d9d3564104786568d26cec14cafa
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-07-02 21:34:46 -0700

    Updates

--- precache-manifest.cc25ba7a864932a3f2d6e2f123e4d156.js
+++ precache-manifest.cc25ba7a864932a3f2d6e2f123e4d156.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "59139820f82a88638355",
+    "revision": "e70da8df5c6435adae9f",
     "url": "/new-mern-practice/static/css/main.9a555ecc.chunk.css"
   },
   {
-    "revision": "59139820f82a88638355",
-    "url": "/new-mern-practice/static/js/main.5a117b20.chunk.js"
+    "revision": "e70da8df5c6435adae9f",
+    "url": "/new-mern-practice/static/js/main.914563e5.chunk.js"
   },
   {
     "revision": "c5893241a6eb6858c7e0",
     "url": "/new-mern-practice/static/js/runtime~main.32ec5198.js"
   },
   {
-    "revision": "88368767af0c92ac0b87",
-    "url": "/new-mern-practice/static/js/2.3f2a9c10.chunk.js"
+    "revision": "dd16e2d92d67c5bf0dec",
+    "url": "/new-mern-practice/static/js/2.2c93782b.chunk.js"
   },
   {
     "revision": "ab2c3997a259adc09a47f03f7f920b16",
     "url": "/new-mern-practice/static/media/profilePicture.ab2c3997.jpg"
   },
   {
-    "revision": "41ecff045ece0c7b96af6b4aa37d2389",
+    "revision": "e57003d0d10788c6992558a9612985cf",
     "url": "/new-mern-practice/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/new-mern-practice/precache-manifest.6e856f79917598bb633b064408b41e9f.js"
+  "/new-mern-practice/precache-manifest.cc25ba7a864932a3f2d6e2f123e4d156.js"
 );
 
 workbox.clientsClaim();
